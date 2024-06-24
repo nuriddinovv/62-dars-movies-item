53c794dc4bdc9f4277e5502c2bb57057e003d99a
nuriddinovv <blackscarp1onoff@gmail.com>
2024-06-24 18:51:24 +0500

    aa

--- src/pages/Movies/MovieById.jsx
+++ src/pages/Movies/MovieById.jsx
@@ -24,6 +24,9 @@ function MovieById() {
       console.error("Error fetching movie data:", error);
     } finally {
       setLoader(false);
+      if (selectedMovie) {
+        console.log(selectedMovie);
+      }
     }
   };
 
@@ -81,16 +84,46 @@ function MovieById() {
             alt={selectedMovie.title}
           />
           <div className="text">
-            <h1>{selectedMovie.title}</h1>
-            <span>
-              <CustomCircularProgress
-                value={Math.round(selectedMovie.vote_average * 10)}
-              />
+            <h1>
+              {selectedMovie.title}{" "}
+              <span style={{ fontWeight: "400" }}>
+                ({new Date(selectedMovie.release_date).getFullYear()})
+              </span>
+            </h1>
+            <div className="rate">
+              <span style={{ display: "flex", gap: "10px", justifyContent: 'center', alignItems:'center'}}>
+                <CustomCircularProgress
+                  value={Math.round(selectedMovie.vote_average * 10)}
+                />
+
+                {lang === "en"
+                  ? `Score`
+                  : "Рейтинг"}
+              </span>
+              <div className="smile">
+                <img
+                  src="https://www.themoviedb.org/assets/2/v8/1f92f-a18cb233c7639241a00dd2fea97c74a12765c05a55b881653868dad147165eda.svg"
+                  alt=""
+                />
+                <img
+                  src="https://www.themoviedb.org/assets/2/v8/1f600-f53b445a86235a4ef54899ad2f1a936e3ff6d1dcdaafc9ed63d6a6070491c0a1.svg"
+                  alt=""
+                />
+                <img
+                  src="https://www.themoviedb.org/assets/2/v8/1f60d-f12478ffe50d98da9d6cafbf29ef1777b8d1d2bb123224c978ca9ba4e6e6159b.svg"
+                  alt=""
+                />
+              </div>
+              <div className="vibe">
+                <button>What's your Vibe ?</button>
+              </div>
+            </div>
+            <div className="desc">
               <h2>{lang === "en" ? "Overview" : "Обзор"}</h2>
               <p style={{ width: "700px" }}>
                 {selectedMovie.overview ? selectedMovie.overview : "Not found"}
               </p>
-            </span>
+            </div>
           </div>
         </div>
       </div>
